30547fc394a3389527c1a852472ae403d22ca0ea
derek <2865900737@qq.com>
2019-11-10 22:01:13 +0800

    update build dist

--- html/config/index.js
+++ html/config/index.js
@@ -9,7 +9,7 @@
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
-  window.SITE_CONFIG['version'] = '1911061051';   // 版本号(年月日时分)
+  window.SITE_CONFIG['version'] = '1911102152';   // 版本号(年月日时分)
   window.SITE_CONFIG['cdnUrl']  = window.SITE_CONFIG.domain + window.SITE_CONFIG.version;
 })();
 

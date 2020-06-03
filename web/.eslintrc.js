9cf561d82e28652a843c45c15e1434798f7d6bb2
penghcheng <375488452@qq.com>
2020-06-03 16:59:54 +0800

    update

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-tabs': 'off'
   }
 }

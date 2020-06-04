a3767c1e8b24f57023c169b87e047ae3b77f60b2
penghcheng <2865900737@qq.com>
2020-06-04 22:43:01 +0800

    update

---
+++ api/app/Common/Dao/SysUserDao.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: derek
+ * Date: 2020/6/4
+ * Time: 22:29
+ */
+
+namespace App\Common\Dao;
+
+
+class SysUserDao extends BaseDao
+{
+
+}
---
+++ api/app/Service/BaseService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: derek
+ * Date: 2020/6/4
+ * Time: 22:27
+ */
+
+namespace App\Service;
+
+
+class BaseService
+{
+
+}
